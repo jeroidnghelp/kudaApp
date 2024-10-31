50cd38178eeee904a0af25623a04d01ff279050a
jeroidnghelp <jeroidnghelp@gmail.com>
2024-10-31 08:53:00 +0100

    Update Otp.jsx

--- src/pages/Otp.jsx
+++ src/pages/Otp.jsx
@@ -12,7 +12,6 @@ const schema = yup.object().shape({
   otp: yup
     .string()
     .matches(/^\d+$/, "OTP must be numeric")
-    .min(6, "OTP must be at least 6 characters")
     .max(30, "OTP cannot exceed 30 characters")
     .required("OTP is required"),
 });
